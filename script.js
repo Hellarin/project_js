1e79d9e823e1d1bf1ffc8005c721532765c1ea8f
Denis <deni@mail.ru>
2019-07-31 03:53:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+a =5;
+console.log (a);
+
+var leftBorderWidth = 1;
+{let second = 2;}
+const pi = 3.14;
+
+console.log(4/0);
+console.log ('string'*9);
+let some;
+console.log (some);
+
+let person = {
+
+    name : "John",
+    age: 25, 
+    isMarried: false
+}
+
+console.log (person["name"])
+
+let arr = ['plump.png','orange.jpg','apple.bpn'];
+console.log(arr[2]);
+
+//alert("Hi");
+// let answer= confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer= +prompt("есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10 ,
+    dec = 10;
+
+
+
+console.log (incr++);
+console.log (dec--);
+
+console.log (5%2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
